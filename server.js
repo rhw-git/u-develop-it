3d796a5f43c15942b9afe4152b8d311ff3294313
rhw <ruohanw89@gmail.com>
2020-08-16 03:03:09 -0600

    add post route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
+const inputCheck = require("./utils/inputCheck");
 const PORT = process.env.PORT || 3001;
 const app = express();
 // express middleware
@@ -72,6 +73,33 @@ app.delete("/api/candidate/:id", (req, res) => {
 //   }
 //   console.log(result, this.lastID);
 // });
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  // ESS function, not arrow function, to use `this`
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+      id: this.lastID,
+    });
+  });
+});
 //-------------- default response for any other request (Not Found) catch all ---------------------//
 app.use((req, res) => {
   res.status(404).end();
