f9a76ebe70414364d12da80226f22ec1f085acc2
rhw <ruohanw89@gmail.com>
2020-08-16 01:59:35 -0600

    create server api

--- server.js
+++ server.js
@@ -1,15 +1,85 @@
+const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// default response for any other request (Not Found) catch all
+// connect o database
+const db = new sqlite3.Database("./db/election.db", (err) => {
+  if (err) {
+    return console.log(err.message);
+  }
+  console.log("connected to the election database.");
+});
+// request all elements in the database
+app.get("/api/candidates", (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+//---------------- request a single candidate ---------------------//
+app.get("/api/candidate/:id", (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id =?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+//--------------- request to delete a candidate -------------------//
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this, this.changes);
+// });
+app.delete("/api/candidate/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id =?`;
+  const params = [req.params.id];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+    }
+    res.json({
+      message: "successfully deleted",
+      data: this.changes,
+    });
+  });
+});
+// ------------- create a candidate -------------------------//
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//               VALUES (?,?,?,?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function (err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
+//-------------- default response for any other request (Not Found) catch all ---------------------//
 app.use((req, res) => {
   res.status(404).end();
 });
 
-// start eh express.js server at ver end of the server.js
-app.listen(PORT, () => {
-  console.log(`server runing on port ${PORT}`);
+// start eh express.js server at ver end of the server.js. start it after the DB connection
+db.on("open", () => {
+  app.listen(PORT, () => {
+    console.log(`server runing on port ${PORT}`);
+  });
 });
