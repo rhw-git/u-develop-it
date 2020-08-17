07f68199e9bec131f9fa294d8682c1a17162c3d5
rhw <ruohanw89@gmail.com>
2020-08-16 20:17:05 -0600

    set all api

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ const db = new sqlite3.Database("./db/election.db", (err) => {
 });
 // request all elements in the database
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name
+               AS party_name
+               FROM candidates
+               LEFT JOIN parties
+               ON candidates.party_id = parties.id`;
   const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
@@ -30,7 +34,12 @@ app.get("/api/candidates", (req, res) => {
 });
 //---------------- request a single candidate ---------------------//
 app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id =?`;
+  const sql = `SELECT candidates.*, parties.name 
+               AS party_name
+               FROM candidates
+               LEFT JOIN parties
+               ON candidates.party_id = parties.id
+               WHERE candidates.id =?`;
   const params = [req.params.id];
   db.get(sql, params, (err, row) => {
     if (err) {
@@ -43,12 +52,6 @@ app.get("/api/candidate/:id", (req, res) => {
   });
 });
 //--------------- request to delete a candidate -------------------//
-// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result, this, this.changes);
-// });
 app.delete("/api/candidate/:id", (req, res) => {
   const sql = `DELETE FROM candidates WHERE id =?`;
   const params = [req.params.id];
@@ -63,16 +66,6 @@ app.delete("/api/candidate/:id", (req, res) => {
   });
 });
 // ------------- create a candidate -------------------------//
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//               VALUES (?,?,?,?)`;
-// const params = [1, "Ronald", "Firbank", 1];
-// // ES5 function, not arrow function, to use this
-// db.run(sql, params, function (err, result) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result, this.lastID);
-// });
 app.post("/api/candidate", ({ body }, res) => {
   const errors = inputCheck(
     body,
@@ -97,6 +90,69 @@ app.post("/api/candidate", ({ body }, res) => {
       message: "success",
       data: body,
       id: this.lastID,
+    });
+  });
+});
+// ------------------------ api for all parties -------------------------//
+app.get("/api/parties", (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+// ------------------------ api for one party by id -------------------------//
+app.get("/api/party/:id", (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id =?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+// ------------------------ api for delete one party -------------------------//
+app.delete("/api/party/:id", (req, res) => {
+  const sql = `DELETE FROM parties WHERE id =?`;
+  const params = [req.params.id];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+    res.json({ message: "successfully deleted", changes: this.changes });
+  });
+});
+// ------------------------ api update party affiliation -------------------------//
+app.put("/api/candidate/:id", (req, res) => {
+  const errors = inputCheck(req.body, "party_id");
+  if (errors) {
+    res.status(400).json({ error: errors });
+  }
+  const sql = `UPDATE candidates SET party_id = ?
+               WHERE id =?`;
+  const params = [req.body.party_id, req.params.id];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: req.body,
+      changes: this.changes,
     });
   });
 });
